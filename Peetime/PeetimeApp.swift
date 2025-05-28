91dfe75bc51859e7caa9feb3074dfc7b5fcc9385
Rafael Hajjar <rafaelhajjar@Rafaels-MacBook-Pro.local>
2025-05-28 16:02:25 +0100

    Initial project push with structure

---
+++ Peetime/PeetimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeetimeApp.swift
+//  Peetime
+//
+//  Created by Rafael Hajjar on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct PeetimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PeetimeTests/PeetimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PeetimeTests.swift
+//  PeetimeTests
+//
+//  Created by Rafael Hajjar on 5/28/25.
+//
+
+import Testing
+@testable import Peetime
+
+struct PeetimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
